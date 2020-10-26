b4a8ce10ffdef31771dff75e9b6f72e5e0fe890c
Andrew I <greyfoxjob@gmail.com>
2020-10-26 12:31:31 +0600

    Updates

---
+++ precache-manifest.ecd30fd205cdc79d18cddd501165c01f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32ac702800c721e45c16970f2060e88",
+    "url": "/cimpleo-react-test/index.html"
+  },
+  {
+    "revision": "650fc99771f752c8c524",
+    "url": "/cimpleo-react-test/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "04c768db60895b904caf",
+    "url": "/cimpleo-react-test/static/css/main.07d95123.chunk.css"
+  },
+  {
+    "revision": "650fc99771f752c8c524",
+    "url": "/cimpleo-react-test/static/js/2.ae0231fa.chunk.js"
+  },
+  {
+    "revision": "19fab4da75f2d84a713b2ff1d1944db4",
+    "url": "/cimpleo-react-test/static/js/2.ae0231fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04c768db60895b904caf",
+    "url": "/cimpleo-react-test/static/js/main.eca96a05.chunk.js"
+  },
+  {
+    "revision": "9976e8bede4c7385ff68",
+    "url": "/cimpleo-react-test/static/js/runtime-main.c0497bdc.js"
+  },
+  {
+    "revision": "d7abc70c218ba4c8469aab14cb9aa456",
+    "url": "/cimpleo-react-test/static/media/peoples.d7abc70c.svg"
+  },
+  {
+    "revision": "c4b2bf3c9208fab26db7a093b706cf9e",
+    "url": "/cimpleo-react-test/static/media/table.c4b2bf3c.svg"
+  }
+]);
